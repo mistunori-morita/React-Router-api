5eb742d5c38b705bb6f9823eb87397f4dec1f939
mitsunori.morita <mitsunori.morita@speee.jpmitsunori.morita@speee.jpmitsunori.morita@speee.jp>
2018-04-10 12:02:52 +0900

    function component

--- src/index.js
+++ src/index.js
@@ -4,12 +4,5 @@ import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-
-const getDate = () => {
-  const date = new Date();
-  return date.toDateString();
-}
-
-const greeeting = <h1>Hello World date: {getDate()}</h1>
-ReactDOM.render(greeeting, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
